b07dac116b4dda6e0b0ec4772dfa90d2c30d3765
Daniele Martini <daniele@Mac.homenet.telecomitalia.it>
2025-08-14 18:27:12 +0200

    Version 1.0

---
+++ main.js
@@ -0,0 +1,13 @@
+const menuToggle = document.querySelector('.toggle');
+const showcase = document.querySelector('.showcase');
+
+menuToggle.addEventListener('click', () => {
+  menuToggle.classList.toggle('active');
+  showcase.classList.toggle('active');
+})
+
+fetch("footer.html")
+  .then(res => res.text())
+  .then(html => {
+    document.getElementById("footer").innerHTML = html;
+  });
